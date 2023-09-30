b4ca67931b1d4fb734a6aefe6f6cf087c7e8b9d5
Hepengzhu <2949430899@qq.com>
2023-09-30 16:38:09 +0800

    实现鼠标移动图片移动的效果

---
+++ BiliBili头部导航图片鼠标滑动效果/index.js
@@ -0,0 +1,9 @@
+// 获取 DOM元素
+const header = document.querySelector('header')
+// 绑定鼠标移动事件
+header.addEventListener('mousemove',(e)=>{
+    // 计算鼠标相对于浏览器窗口宽度的百分比 (最左边的时候是0 ，最右边的时候是1)
+    let percentage = e.clientX / window.outerWidth
+    // 将百分比给css变量
+    header.style.setProperty('--percentage',percentage)
+})
